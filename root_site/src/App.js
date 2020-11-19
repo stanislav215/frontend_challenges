ea1375cd03fd1850deb22367577a058225b97a58
sm492am <stanislav.macak@student.tuke.sk>
2020-11-19 18:40:53 +0100

    root site

--- root_site/src/App.js
+++ root_site/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
       <main>
         <div className="sites">
-          <Site img="./dec2bin.png" href="https://sleepy-keller-0faf33.netlify.app" />
+          <Site img="./dec2bin.png" href="https://keen-mcnulty-85a198.netlify.app" />
           <Site img="./coming-soon.png" href="#"/>
           <Site img="./coming-soon.png" href="#"/>
           <Site img="./coming-soon.png" href="#"/>
