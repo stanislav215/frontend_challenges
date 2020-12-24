bb8a375f06f3353b95a540c6b39ed950eca22a30
sm492am <stanislav.macak@student.tuke.sk>
2020-12-25 00:04:36 +0100

    adding pasgen

--- root_site/src/App.js
+++ root_site/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <main>
         <div className="sites">
           <Site img="./dec2bin.png" href="https://loving-swanson-66f3e0.netlify.app" />
-          <Site img="./coming-soon.png" href="#"/>
+          <Site img="./passgen.png" href="https://musing-ptolemy-15169c.netlify.app/"/>
           <Site img="./coming-soon.png" href="#"/>
           <Site img="./coming-soon.png" href="#"/>
           <Site img="./coming-soon.png" href="#"/>
